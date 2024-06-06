0414f5753a3397c0642bb44e1aa5ca63f791a24b
Ares <89419093+arestosora@users.noreply.github.com>
2024-06-06 01:13:08 -0500

    update: update packages & pattern command (#35)

--- src/commands/pattern/meow.ts
+++ src/commands/pattern/meow.ts
@@ -1,13 +1,14 @@
 import type { Message } from 'discord.js';
 import { PatternCommand } from '@sapphire/plugin-pattern-commands';
 import { ApplyOptions } from '@sapphire/decorators';
+import { resolveKey } from '@sapphire/plugin-i18next';
 
 @ApplyOptions<PatternCommand.Options>({
 	aliases: ['gato', 'cat'],
 	chance: 1
 })
 export class MeowCommand extends PatternCommand {
-	public messageRun(message: Message) {
-		message.reply('Meow! You got meowd with a 1% chance! ❤️');
+	public async messageRun(message: Message) {
+		message.reply({ content: await resolveKey(message, 'commands/replies/pattern:meow') });
 	}
 }
