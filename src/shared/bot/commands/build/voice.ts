9c0d7f062afe6b5f215d5548e1be367ad19ad0f3
gacarbla <gacarbla@gacarbla.es>
2024-06-15 16:04:59 +0200

    Implemented command-resgister: /voice (#46)

--- src/shared/bot/commands/build/voice.ts
+++ src/shared/bot/commands/build/voice.ts
@@ -1,41 +1,32 @@
 import { Subcommand } from '@sapphire/plugin-subcommands';
-import { applyLocalizedBuilder } from '@sapphire/plugin-i18next';
+import { CommandRegister } from '../../../classes/CommandRegister';
 
 export class VoiceSubCommandsRegistration {
     public static registerCommands(registry: Subcommand.Registry): void {
-        registry.registerChatInputCommand((builder) => 
-            applyLocalizedBuilder(builder, 'commands/names:voice', 'commands/descriptions:voice')
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:name', 'commands/descriptions/voice:name'))
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:claim', 'commands/descriptions/voice:claim'))
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:ghost', 'commands/descriptions/voice:ghost'))
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:unghost', 'commands/descriptions/voice:unghost'))
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:reset', 'commands/descriptions/voice:reset'))
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:lock', 'commands/descriptions/voice:lock'))
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:unlock', 'commands/descriptions/voice:unlock'))
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:limit', 'commands/descriptions/voice:limit')
-                    .addIntegerOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:limit_name', 'commands/options/voice:limit_description').setRequired(true))
-                )
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:permit', 'commands/descriptions/voice:permit')
-                    .addUserOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:permit_name', 'commands/options/voice:permit_description').setRequired(true))
-                )
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:reject', 'commands/descriptions/voice:reject')
-                    .addUserOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:reject_name', 'commands/options/voice:reject_description').setRequired(true))
-                )
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:transfer', 'commands/descriptions/voice:transfer')
-                    .addUserOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:transfer_name', 'commands/options/voice:transfer_description').setRequired(true))
-                )
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:invite', 'commands/descriptions/voice:invite')
-                    .addUserOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:invite_name', 'commands/options/voice:invite_description').setRequired(true))
-                )
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:bitrate', 'commands/descriptions/voice:bitrate')
-                    .addIntegerOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:bitrate', 'commands/options/voice:bitrate_description').setRequired(true))
-                )
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:trust', 'commands/descriptions/voice:trust')
-                    .addUserOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:trust_name', 'commands/options/voice:trust_description').setRequired(true))
-                )
-                .addSubcommand((command) => applyLocalizedBuilder(command, 'commands/names/voice:untrust', 'commands/descriptions/voice:untrust')
-                    .addUserOption((option) => applyLocalizedBuilder(option, 'commands/options/voice:untrust_name', 'commands/options/voice:untrust_description').setRequired(true))
-                )
-        )
+        registry.registerChatInputCommand((builder) => {
+            const register = new CommandRegister({
+                key: "voice",
+                subcommands: [
+                    { key: 'name' },
+                    { key: 'claim' },
+                    { key: 'ghosted',  options: [{ type: 'string',  key: 'statusGhosted',  required: true,
+                        choices: [{ key: 'n', value: 'n' }, { key: 'y', value: 'y' }]
+                    }]},
+                    { key: 'reset' },
+                    { key: 'locked',   options: [{ type: 'string',  key: 'statusLocked',   required: true,
+                        choices: [{ key: 'n', value: 'n' }, { key: 'y', value: 'y' }]
+                    }]},
+                    { key: 'limit',    options: [{ type: 'integer', key: 'limit',          required: true, }] },
+                    { key: 'permit',   options: [{ type: 'user',    key: 'userPermit',     required: true, }] },
+                    { key: 'reject',   options: [{ type: 'user',    key: 'userReject',     required: true, }] },
+                    { key: 'transfer', options: [{ type: 'user',    key: 'userTransfer',   required: true, }] },
+                    { key: 'invite',   options: [{ type: 'user',    key: 'userInvite',     required: true, }] },
+                    { key: 'bitrate',  options: [{ type: 'integer', key: 'bitrateBitrate', required: true, }] },
+                    { key: 'trust',    options: [{ type: 'user',    key: 'userTrust',      required: true, }] },
+                    { key: 'untrust',  options: [{ type: 'user',    key: 'userUntrust',    required: true, }] },
+                ]
+            })
+            return register.build(builder)
+        });
     }
 }
