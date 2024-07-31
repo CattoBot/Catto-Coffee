af760e9c15754bd31310eecea77a615b1c1d92f3
gacarbla <gacarbla@gacarbla.es>
2024-08-01 00:19:27 +0200

    Update: Use of i18 service (#46)

--- src/shared/classes/CommandRegister.ts
+++ src/shared/classes/CommandRegister.ts
@@ -1,22 +1,10 @@
 import { SlashCommandBuilder, SlashCommandSubcommandBuilder, SlashCommandSubcommandGroupBuilder } from 'discord.js';
 import { applyLocalizedBuilder, createLocalizedChoice } from '@sapphire/plugin-i18next';
 import { CattoCommandObject, CattoSubcommandGroupObject, CattoCommandOptionObject, CattoSubcommandObject } from '../types/Commands';
+import { container } from '@sapphire/pieces';
 
 export class CommandRegister {
     private cmdObj;
-    private i18routes = {
-        name: ":$cmd",
-        description: ":$cmd",
-        subcommands: "/$type_$cmd:$key",
-        subcommandgroups: "/$type_$cmd:$key",
-        options: "/$type_$cmd:$key"
-    }
-    private geti18CmdRt(pk: { commandName: string, type: 'name' | 'description' | 'subcommandgroups' | 'subcommands' | 'options', ext?: { type: 'name' | 'description' | 'choices', key: string } }) {
-        return `commands/${pk.type}${this.i18routes[pk.type]}`
-            .replace("$cmd", pk.commandName)
-            .replace("$type", pk.ext?.type || '')
-            .replace("$key", pk.ext?.key || '')
-    }
 
     constructor(CattoCommandObject: CattoCommandObject) {
         this.cmdObj = CattoCommandObject;
@@ -30,8 +18,8 @@ export class CommandRegister {
         ]
         if ( eOpts.filter(n => !!n).length > 1 )
             throw new Error("O opciones, o grupos de subcomandos o subcomandos, pero más de uno no chico...")
-        let cmdLocalName = this.geti18CmdRt({ commandName: this.cmdObj.key, type: 'name' }),
-            cmdLocalDesc = this.geti18CmdRt({ commandName: this.cmdObj.key, type: 'description' })
+        let cmdLocalName = container.services.i18.getCommandRoute({ commandName: this.cmdObj.key, type: 'name' }),
+            cmdLocalDesc = container.services.i18.getCommandRoute({ commandName: this.cmdObj.key, type: 'description' })
         const localizedBuilder = applyLocalizedBuilder(commandbuilder, cmdLocalName, cmdLocalDesc);
         if (this.cmdObj.options) {
             for (const optcmdObj of this.cmdObj.options) {
@@ -52,8 +40,8 @@ export class CommandRegister {
 
     private addOption(builder: SlashCommandBuilder | SlashCommandSubcommandBuilder, option: CattoCommandOptionObject): void {
         let opt;
-        let optLocalName = this.geti18CmdRt({ commandName: this.cmdObj.key, type: 'options', ext: { key: option.key, type: 'name' } }),
-            optLocalDesc = this.geti18CmdRt({ commandName: this.cmdObj.key, type: 'options', ext: { key: option.key, type: 'description' } })
+        let optLocalName = container.services.i18.getCommandRoute({ commandName: this.cmdObj.key, type: 'options', ext: { key: option.key, type: 'name' } }),
+            optLocalDesc = container.services.i18.getCommandRoute({ commandName: this.cmdObj.key, type: 'options', ext: { key: option.key, type: 'description' } })
         switch (option.type) {
             case 'string':
                 builder.addStringOption((build_option) => {
@@ -62,7 +50,7 @@ export class CommandRegister {
                     option.choices?.forEach(choice => {
                         process.addChoices(
                             createLocalizedChoice(
-                                this.geti18CmdRt({ commandName: this.cmdObj.key, type: 'options', ext: { key: choice.key, type: 'choices' } }),
+                                container.services.i18.getCommandRoute({ commandName: this.cmdObj.key, type: 'options', ext: { key: choice.key, type: 'choices' } }),
                                 { value: choice.value }
                             )
                         )
@@ -93,12 +81,12 @@ export class CommandRegister {
     }
 
     private addSubcommand = (builder: SlashCommandBuilder | SlashCommandSubcommandGroupBuilder, subcommand: CattoSubcommandObject) => {
-        if (!this.cmdObj.subcommands?.filter(n => n)) throw new Error("Se ha intentado crear subcomandos sin subcomandos. wtf?")
+        if (this.cmdObj.subcommands?.filter(n => n).length == 0) throw new Error("Se ha intentado crear subcomandos sin subcomandos. wtf?")
         builder.addSubcommand((sbcmd) => {
             const subcommandBuilder = applyLocalizedBuilder(
                 sbcmd,
-                this.geti18CmdRt({commandName: this.cmdObj.key, type:'subcommands', ext:{ type: 'name', key: subcommand.key }}),
-                this.geti18CmdRt({commandName: this.cmdObj.key, type:'subcommands', ext:{ type: 'description', key: subcommand.key }})
+                container.services.i18.getCommandRoute({commandName: this.cmdObj.key, type:'subcommands', ext:{ type: 'name', key: subcommand.key }}),
+                container.services.i18.getCommandRoute({commandName: this.cmdObj.key, type:'subcommands', ext:{ type: 'description', key: subcommand.key }})
             )
             if (subcommand.options) {
                 for (const option of subcommand.options) {
@@ -110,12 +98,12 @@ export class CommandRegister {
     }
 
     private addSubcommandGroup = (builder: SlashCommandBuilder, subcommandgroup: CattoSubcommandGroupObject) => {
-        if (!this.cmdObj.subcommandgroups?.filter(n => n)) throw new Error("Se  ha intentado crear grupos de subcomandos sin grupos de subcomandos. wtf?")
+        if (this.cmdObj.subcommandgroups?.filter(n => n).length == 0) throw new Error("Se  ha intentado crear grupos de subcomandos sin grupos de subcomandos. wtf?")
         builder.addSubcommandGroup((sbcmdgr) => {
             const subcommandgroupBuilder = applyLocalizedBuilder(
                 sbcmdgr,
-                this.geti18CmdRt({commandName: this.cmdObj.key, type: 'subcommandgroups', ext: { key: subcommandgroup.key, type: 'name' }}),
-                this.geti18CmdRt({commandName: this.cmdObj.key, type: 'subcommandgroups', ext: { key: subcommandgroup.key, type: 'description' }})
+                container.services.i18.getCommandRoute({commandName: this.cmdObj.key, type: 'subcommandgroups', ext: { key: subcommandgroup.key, type: 'name' }}),
+                container.services.i18.getCommandRoute({commandName: this.cmdObj.key, type: 'subcommandgroups', ext: { key: subcommandgroup.key, type: 'description' }})
             )
             for (const subcommand of subcommandgroup.subcommands) {
                 this.addSubcommand(subcommandgroupBuilder, subcommand)
