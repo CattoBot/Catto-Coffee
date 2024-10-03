c3683d9693dab5e870e119a2c9120f8baf455f10
gacarbla <gacarbla@gacarbla.es>
2024-10-03 17:59:09 +0200

    Added: Restored old LeaderboardType enum (#55)

--- src/lib/enum/index.ts
+++ src/lib/enum/index.ts
@@ -1,12 +1,14 @@
 import EMOJIS from "./emojis";
 import LOGTYPE from "./logtype";
 import STATUS from "./status";
+import LEADERBOARDTYPE from "./LeaderboardType";
 
 const Enumerations = {
     EMOJIS: EMOJIS,
     LOGTYPE: LOGTYPE,
-    STATUS: STATUS
+    STATUS: STATUS,
+    LEADERBOARDTYPE: LEADERBOARDTYPE
 }
 
 export default Enumerations
-export { EMOJIS, LOGTYPE, STATUS }
+export { EMOJIS, LOGTYPE, STATUS, LEADERBOARDTYPE }
